bb6a8dddb8bbdabd00687636564965e2c7356e20
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-06-15 13:03:24 +0300

    commit tree b64aa5e648a74624f5d1d9bb078e9c36f9a3f665

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your code here
+//1. SEND DATA
+//=============
+const userCredentials = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+const dataSubmission = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(userCredentials),
+};
+
+//2. HANDLE RESPONSE
+//===================
+
+function submitData() {
+  return (
+    fetch("http://localhost:3000/users", dataSubmission)
+      .then((res) => res.json())
+      .then((data) => {
+        let input = document.querySelector("body");
+        input.textContent = data.id;
+      })
+      //3. HANDLE ERRORS
+      //=================
+      .catch((error) => {
+        let errorMessage = document.querySelector("body");
+        errorMessage.textContent = error.message;
+      })
+  );
+}
+// submitData("Sam", "sam@sam.com");
